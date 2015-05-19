ee6dcb1a065b1842e84b68255771b98170d04864
Romain PELISSE <belaran@gmail.com>
2015-05-19 09:18:47 +0200

    bump default version to 0.5.0.Final

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-readonly BUGCLERK_VERSION=${BUGCLERK_VERSION:-'0.4.5.Final'}
+readonly BUGCLERK_VERSION=${BUGCLERK_VERSION:-'0.5.0.Final'}
 readonly FILTER_URL=${FILTER_URL:-'https://bugzilla.redhat.com//buglist.cgi?cmdtype=dorem&amp;list_id=3303150&amp;namedcmd=jboss-eap-6.4.z-superset&amp;remaction=run&amp;sharer_id=213224&amp;ctype=csv'}
 
 mvn exec:java "-Dbugclerk.version=${BUGCLERK_VERSION}" "-Dbugclerk.filter.url=${FILTER_URL}"

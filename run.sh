9358af6a1408b24b2cdc0e0034ddcec7d51e9b26
Romain Pelisse <belaran@gmail.com>
2021-07-20 17:45:52 +0200

    Ensures config is overrideable in run.sh

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-readonly APHRODITE_CONFIG_FILE=$(pwd)/aphrodite-config.json
+readonly APHRODITE_CONFIG_FILE=${APHRODITE_CONFIG_FILE:-$(pwd)/aphrodite-config.json}
 
 if [ ! -e "${APHRODITE_CONFIG_FILE}" ]; then
   echo "Aphrodite config file does not exist: ${APHRODITE_CONFIG_FILE}"
   exit 1
 fi
 
-mvn "-Daphrodite.config=${APHRODITE_CONFIG_FILE}" exec:java
+mvn -U "-Daphrodite.config=${APHRODITE_CONFIG_FILE}" exec:java
